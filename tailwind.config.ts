34a417516b8c373b90990bb339cb22ebcd9ab3f2
pioneer2077 <134494401+pioneer2077@users.noreply.github.com>
2023-10-30 17:22:23 +0900

    responsive navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+
+    minWidth: {
+      "244px": "244px",
+      "48px": "48px",
+    },
+    minHeight: {
+      "100vh": "100vh",
+    },
+    backgroundColor: {
+      primary: "#4193ef",
+      secondary: "#f2f2f2",
+    },
+    textColor: {
+      blue: "#4193ef",
+      white: "#ffffff",
+    },
+
+    screens: {
+      tablet: "719px",
+      lap: "1152px",
+      desktop: "1260px",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
